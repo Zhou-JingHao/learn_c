aa8c8e04e44ea0c621c51fbe4ac65b1fde5112de
Zhou-JingHao <20327404@qq.com>
2022-06-23 20:18:50 +0800

    up

--- text.c
+++ text.c
@@ -1,9 +1,24 @@
+
 #include <stdio.h>
-#include <stdlib.h>
-int main()
+ 
+int max(int x, int y)
 {
-    printf("Hello World!\n");
-    printf("你好世界！\n");
+    return x > y ? x : y;
+}
+ 
+int main(void)
+{
+    /* p 是函数指针 */
+    int (* p)(int, int) = & max; // &可以省略
+    int a, b, c, d;
+ 
+    printf("请输入三个数字:");
+    scanf("%d %d %d", & a, & b, & c);
+ 
+    /* 与直接调用函数等价，d = max(max(a, b), c) */
+    d = p(p(a, b), c); 
+ 
+    printf("最大的数字是: %d\n", d);
+ 
     system("pause");
-    return 0;
 }
